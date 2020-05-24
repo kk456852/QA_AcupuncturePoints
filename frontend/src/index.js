60bdd251b41b1197dcdc6e7961f9e2cbe021dee0
kk456852 <931384141@qq.com>
2020-05-24 12:28:37 +0800

    最后的提交

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import main from './pages/main'
 import login from './pages/login'
 
